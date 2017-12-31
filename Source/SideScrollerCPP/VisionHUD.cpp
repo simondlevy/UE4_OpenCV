2089e1c65193bd46789169cd35e97e3ba8b9c7e4
simondlevy <simon.d.levy@gmail.com>
2017-12-30 20:51:15 -0500

    Draw HUD in lower right with yellow border

--- Source/SideScrollerCPP/VisionHUD.cpp
+++ Source/SideScrollerCPP/VisionHUD.cpp
@@ -34,25 +34,23 @@ void AVisionHUD::DrawHUD()
 	// Read the pixels from the RenderTarget and store them in a FColor array
 	MiniMapRenderTarget->ReadPixels(MiniMapSurfData);
 
-	for (int x = 0; x < MiniMapTextureRenderTarget->SizeX; ++x) {
+	int rows = MiniMapTextureRenderTarget->SizeY;
+	int cols = MiniMapTextureRenderTarget->SizeX;
 
-		for (int y = 0; y < MiniMapTextureRenderTarget->SizeY; ++y) {
+	for (int x = 0; x < cols; ++x) {
 
-			int k = x + y * MiniMapTextureRenderTarget->SizeX;
+		for (int y = 0; y < rows; ++y) {
+
+			int k = x + y * cols;
 
 			FColor PixelColor = MiniMapSurfData[k];
 
-			DrawRect(PixelColor, x, y, 1, 1);
+			DrawRect(PixelColor, LEFTX+x, TOPY+y, 1, 1);
 
 			// RGB->gray formula from https ://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/
 			//imgdata[k] = (byte)(0.21 *PixelColor.R + 0.72 * PixelColor.G + 0.07 * PixelColor.B);
 		}
 	}
-
-/*
-
-	int rows = MiniMapTextureRenderTarget->SizeY;
-	int cols = MiniMapTextureRenderTarget->SizeX;
 
 	float rightx = LEFTX + WIDTH;
 	float bottomy = TOPY + HEIGHT;
@@ -60,7 +58,7 @@ void AVisionHUD::DrawHUD()
 	drawBorder(LEFTX, TOPY, rightx, TOPY);
 	drawBorder(rightx, TOPY, rightx, bottomy);
 	drawBorder(rightx, bottomy, LEFTX, bottomy);
-	drawBorder(LEFTX, bottomy, LEFTX, TOPY);*/
+	drawBorder(LEFTX, bottomy, LEFTX, TOPY);
 }
 
 void AVisionHUD::drawBorder(float lx, float uy, float rx, float by)
