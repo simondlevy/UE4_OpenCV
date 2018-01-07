5ba3158692c785d2c10678a4447005ed9f9e9130
simondlevy <simon.d.levy@gmail.com>
2018-01-07 13:16:14 -0500

    Begin OpenCV integration

--- Source/SideScrollerCPP/SideScrollerCPP.Build.cs
+++ Source/SideScrollerCPP/SideScrollerCPP.Build.cs
@@ -1,13 +1,64 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
+
 
 public class SideScrollerCPP : ModuleRules
 {
-	public SideScrollerCPP(ReadOnlyTargetRules Target) : base(Target)
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/")); }
+    }
+
+    public SideScrollerCPP(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-	}
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI", "RenderCore", "ShaderCore" });
+
+        LoadOpenCV(Target);
+    }
+
+    public bool LoadOpenCV(ReadOnlyTargetRules Target)
+    {
+        // Start OpenCV linking here!
+        bool isLibrarySupported = false;
+
+        // Create OpenCV Path 
+        string OpenCVPath = Path.Combine(ThirdPartyPath, "OpenCV");
+
+        // Get Library Path 
+        string LibPath = "";
+        bool isdebug = Target.Configuration == UnrealTargetConfiguration.Debug && BuildConfiguration.bDebugBuildsActuallyUseDebugCRT;
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            LibPath = Path.Combine(OpenCVPath, "Libraries", "Win64");
+            isLibrarySupported = true;
+        }
+        else
+        {
+            string Err = string.Format("{0} dedicated server is made to depend on {1}. We want to avoid this, please correct module dependencies.", Target.Platform.ToString(), this.ToString()); System.Console.WriteLine(Err);
+        }
+
+        if (isLibrarySupported)
+        {
+            //Add Include path 
+            PublicIncludePaths.AddRange(new string[] { Path.Combine(OpenCVPath, "Includes") });
+
+            // Add Library Path 
+            PublicLibraryPaths.Add(LibPath);
+
+            //Add Static Libraries
+            PublicAdditionalLibraries.Add("opencv_world340.lib");
+
+            //Add Dynamic Libraries
+            PublicDelayLoadDLLs.Add("opencv_world340.dll");
+            PublicDelayLoadDLLs.Add("opencv_ffmpeg340_64.dll");
+        }
+
+        Definitions.Add(string.Format("WITH_OPENCV_BINDING={0}", isLibrarySupported ? 1 : 0));
+
+        return isLibrarySupported;
+    }
 }
