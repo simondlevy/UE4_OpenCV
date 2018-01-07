e6ec0fbbbbb5936dd90b7ce5db139c6cb3da21df
simondlevy <simon.d.levy@gmail.com>
2018-01-07 15:20:21 -0500

    Optical flow working

--- Source/SideScrollerCPP/OpticalFlow.cpp
+++ Source/SideScrollerCPP/OpticalFlow.cpp
@@ -11,6 +11,9 @@
 #include "OpticalFlow.h"
 #include "OnscreenDebug.h"
 
+#include <opencv2/core.hpp>
+#include <opencv2/video/tracking.hpp>
+
 
 OpticalFlow::OpticalFlow(AHUD* hud, int leftx, int topy, int rows, int cols) : 
 	_leftx(leftx), _topy(topy), _hud(hud),  _rows(rows), _cols(cols)
@@ -34,15 +37,39 @@ void OpticalFlow::perform(uint8_t* imagergb)
 		_imgcurr[k] = (uint8_t)(0.21 * imagergb[3 * k] + 0.72 * imagergb[3 * k + 1] + 0.07 * imagergb[3 * k + 2]);
 	}
 
-	// Fake up optical flow with a line for now
+
+	// Run optical flow after we have a second frame
 	if (_ready) {
 
+		cv::Mat curr(_rows, _cols, CV_8UC1, _imgcurr);
+		cv::Mat prev(_rows, _cols, CV_8UC1, _imgprev);
+
+		cv::Mat flow;
+		cv::calcOpticalFlowFarneback(prev, curr, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+	
+		int skip = 16;
+		float div = 5;
+
+		for (int x = 0; x < _cols; x+=skip) {
+			for (int y = 0; y < _rows; y+= skip) {
+
+				float fx = flow.ptr<float>(y, x)[0] / div;
+				float fy = flow.ptr<float>(y, x)[1] / div;
+
+				int cx = _leftx + x;
+				int cy = _topy + y;
+
+				_hud->Draw2DLine(cx, cy, cx+fx, cy+fy, FColor::Yellow);
+			}
+		}
+
+		/*
 		int16_t ofx = 0;
 		int16_t ofy = 0;
 		ofoLK_Plus_2D(_imgcurr, _imgprev, _rows, _cols, 100, &ofx, &ofy);
-		int ctrx = _leftx + _cols / 2;
-		int ctry = _topy  + _rows / 2;
+
 		_hud->Draw2DLine(ctrx, ctry, ctrx+ofx, ctry+ofy, FColor::Yellow);
+		*/
 	}
 
 	_ready = true;
