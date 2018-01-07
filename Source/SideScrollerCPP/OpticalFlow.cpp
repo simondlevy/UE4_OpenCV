9db7a9734d11f31bf5a930850c458cc4b00884d2
simondlevy <simon.d.levy@gmail.com>
2018-01-07 15:27:11 -0500

    Cleanup

--- Source/SideScrollerCPP/OpticalFlow.cpp
+++ Source/SideScrollerCPP/OpticalFlow.cpp
@@ -62,14 +62,6 @@ void OpticalFlow::perform(uint8_t* imagergb)
 				_hud->Draw2DLine(cx, cy, cx+fx, cy+fy, FColor::Yellow);
 			}
 		}
-
-		/*
-		int16_t ofx = 0;
-		int16_t ofy = 0;
-		ofoLK_Plus_2D(_imgcurr, _imgprev, _rows, _cols, 100, &ofx, &ofy);
-
-		_hud->Draw2DLine(ctrx, ctry, ctrx+ofx, ctry+ofy, FColor::Yellow);
-		*/
 	}
 
 	_ready = true;
@@ -77,61 +69,3 @@ void OpticalFlow::perform(uint8_t* imagergb)
 	// Current image becomes previous image on next iteration
 	memcpy(_imgprev, _imgcurr, _rows*_cols);
 }
-
-// Adapted from https://github.com/simondlevy/ArduEye/blob/master/src/OpticalFlow.cpp
-void OpticalFlow::ofoLK_Plus_2D(uint8_t *curr_img, uint8_t *last_img, uint16_t rows, uint16_t cols, uint16_t scale, int16_t * ofx, int16_t * ofy)
-{
-	int32_t  A11 = 0, A12 = 0, A22 = 0, b1 = 0, b2 = 0;
-	int16_t  F2F1, F4F3, FCF0;
-
-	// set up pointers
-	uint8_t *f0 = curr_img + cols + 1; //center image
-	uint8_t *f1 = curr_img + cols + 2; //right-shifted image
-	uint8_t *f2 = curr_img + cols;     //left-shifted image	
-	uint8_t *f3 = curr_img + 2 * cols + 1; //down-shifted image	
-	uint8_t *f4 = curr_img + 1;		//up-shifted image
-	uint8_t *fz = last_img + cols + 1; 	//time-shifted image
-
-										// loop through
-	for (uint8_t r = 1; r<rows - 1; ++r) {
-
-		for (uint8_t c = 1; c<cols - 1; ++c) {
-
-			// compute differentials, then increment pointers (post 		// increment)
-			F2F1 = (*(f2++) - *(f1++));	//horizontal differential
-			F4F3 = (*(f4++) - *(f3++));	//vertical differential
-			FCF0 = (*(fz++) - *(f0++));	//time differential
-
-										// update summations
-			A11 += (F2F1 * F2F1);
-			A12 += (F4F3 * F2F1);
-			A22 += (F4F3 * F4F3);
-			b1 += (FCF0 * F2F1);
-			b2 += (FCF0 * F4F3);
-		}
-		f0 += 2;	//move to next row of image
-		fz += 2;
-		f1 += 2;
-		f2 += 2;
-		f3 += 2;
-		f4 += 2;
-	}
-
-	//determinant
-	int64_t detA = ((int64_t)(A11)*A22 - (int64_t)(A12)*A12);
-
-	int64_t XS = 0;
-	int64_t YS = 0;
-
-	// Compute final output. Note use of "scale" here to multiply 2*top   
-	// to a larger number so that it may be meaningfully divided using 
-	// fixed point arithmetic	
-	if (detA != 0) {
-		XS = ((int64_t)(b1)*A22 - (int64_t)(b2)*A12) * scale / detA;
-		YS = ((int64_t)(b2)*A11 - (int64_t)(b1)*A12) * scale / detA;
-	}
-
-	(*ofx) = (int16_t)XS;
-	(*ofy) = (int16_t)YS;
-}
-
